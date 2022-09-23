47a92b15c98c017662c8f6c7a5fe9a8c1535d145
xballes <ballesxabi@gmail.com>
2022-09-23 19:29:01 +0200

    Fecha arreglada

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function comprobarCampos(){
     if(document.getElementById('telefono').value.length!=9){
         alert("El numero debe ser de 9 digitos")
     }
-   if(!(/^(\d{4})(-)(0[1-9]|1[012])\2([012][1-9]|3[01])$/.test(document.getElementById('fecha').value))){ //https://foroayuda.es/php-regex-para-verificar-la-fecha-esta-en-formato-aaaa-mm-dd/
+   if(!(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(document.getElementById('fecha').value))){ //https://foroayuda.es/php-regex-para-verificar-la-fecha-esta-en-formato-aaaa-mm-dd/
         alert("La fecha introducida no sigue el patron: aaaa-mm-dd")
    }
     if(!(/^[a-zA-Z]+([\.]?[a-zA-Z0-9_-]+)*@[a-z0-9]+([\.-]+[a-z0-9]+)*\.[a-z]{2,4}$/.test(document.getElementById('email').value))){ //https://es.stackoverflow.com/questions/142/validar-un-email-en-javascript-que-acepte-todos-los-caracteres-latinos
@@ -30,7 +30,7 @@ function dniValido(dni){
     var c = dni.substr(8,1);
      if(!(c.toUpperCase() == 'TRWAGMYFPDXBNJZSQVHLCKET'.charAt(n%23))){
         alert('DNI erroneo');
-    
+
     }
     }else{
         alert('DNI erroneo');
