8055714f92bbedf53d426a924dab768ef3757379
xballes <ballesxabi@gmail.com>
2022-09-24 14:15:35 +0200

    Ya registra los datos de los usuarios

--- registrar.php
+++ registrar.php
@@ -1,29 +1,29 @@
-<?php 
-
-include("conexionbd.php");
-
-if (isset($_POST['registrar'])) {
-    if (strlen($_POST['nombre']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['telefono']) >= 1 && strlen($_POST['fecha']) >= 1 && strlen($_POST['dni']) >= 1) {
-	    $nombre = trim($_POST['name']);
-        $dni = trim($_POST['dni']);
-	    $telefono = trim($_POST['telefono']);
-        $fecha= trim($_POST['fecha']);
-        $email = trim($_POST['email']);
-
-	    $consulta = "INSERT INTO datos(nombre,dni,telefono,fecha,email) VALUES ('$nombre','$dni','$telefono','$fecha','$email')";
-	    $resultado = mysqli_query($conex,$consulta);
-	    if ($resultado) {
-	    	?> 
-	    	<h3 class="correcto">¡Te has registrado correctamente!</h3>
-           <?php
-	    } else {
-	    	?> 
-	    	<h3 class="fallido">¡Ha ocurrido un error!</h3>
-           <?php
-	    }
-    }   else {
-	    	?> 
-	    	<h3 class="fallido">¡Por favor complete los campos!</h3>
-           <?php
-    }
-}
+<?php
+	//conectamos Con el servidor
+	$conectar=@mysqli_connect("localhost","root",'');
+	//verificamos la conexion
+	if(!$conectar){
+		echo"No Se Pudo Conectar Con El Servidor";
+	}else{
+		$base=mysqli_select_db($conectar,"usuarios");
+		if(!$base){
+			echo"No Se Encontro La Base De Datos";			
+		}
+	}
+	//recuperar las variables
+	$nombre=$_POST['nombre'];
+	$dni=$_POST['dni'];
+	$telefono=$_POST['telefono'];
+	$fecha=$_POST['fecha'];
+	$email=$_POST['email'];
+	//hacemos la sentencia de sql
+	$sql="INSERT INTO Usuario VALUES('$nombre','$dni','$telefono','$fecha','$email')";
+	//ejecutamos la sentencia de sql
+	$ejecutar=mysqli_query($conectar,$sql);
+	//verificamos la ejecucion
+	if(!$ejecutar){
+		echo"Hubo Algun Error";
+	}else{
+		echo"Datos Guardados Correctamente";
+	}
+?>
