e0f5492458f761e9cbf0d39fd5741c4075d4b7a5
xballes <ballesxabi@gmail.com>
2022-09-25 12:58:45 +0200

    Logear ya funciona + contraseña

--- logear.php
+++ logear.php
@@ -12,30 +12,17 @@ echo"No Se Encontro La Base De Datos";
 }
 //recuperar las variables
 $nombre=$_POST['nombre'];
-$contrasena=$_POST['contrasena'];
+$contrasena=$_POST['contraseña'];
 //hacemos la sentencia de sql
 $sql="SELECT Contrasena FROM Usuario WHERE (Nombre='$nombre')"; //Consigue la contraseña del usuario
 //ejecutamos la sentencia de sql
 $ejecutar=mysqli_query($conectar,$sql);
 //verificamos la ejecucion
 $cont=mysqli_fetch_array($ejecutar);
-//$result = mysqli_num_rows($ejecutar);
-if(empty($cont[0])){//si no hay ningun resultado... ->establecer la contraseña
-        $establecer="INSERT INTO Usuario (Contrasena) values('$contrasena')";
-$ejecutar=mysqli_query($conectar,$establecer);
-echo"Contraseña establecida correctamente";
-}else{ // Si ya habia establecido la contraseña,se comprueba
-//$cont=mysqli_fetch_array($ejecutar);
 if(strcmp($cont[0],$contrasena) === 0){
 echo "Te has logeado correctamente";
 }else{
 echo "Contraseña incorrecta";
 
 }
-
-
-         
-}
-?>
-
 ?>
--- registrar.php
+++ registrar.php
@@ -1,30 +1,31 @@
 <?php
-	//conectamos Con el servidor
-	$conectar=@mysqli_connect("localhost","root",'');
-	//verificamos la conexion
-	if(!$conectar){
-		echo"No Se Pudo Conectar Con El Servidor";
-	}else{
-		$base=mysqli_select_db($conectar,"usuarios");
-		if(!$base){
-			echo"No Se Encontro La Base De Datos";			
-		}
-	}
-	//recuperar las variables
-	$nombre=$_POST['nombre'];
-	$dni=$_POST['dni'];
-	$telefono=$_POST['telefono'];
-	$fecha=$_POST['fecha'];
-	$email=$_POST['email'];
-	//hacemos la sentencia de sql
-	$sql="INSERT INTO Usuario (Nombre,DNI,Telefono,Fecha,Email) VALUES('$nombre','$dni','$telefono','$fecha','$email')";
-	//ejecutamos la sentencia de sql
-	$ejecutar=mysqli_query($conectar,$sql);
-	//verificamos la ejecucion
-	if(!$ejecutar){
-		echo"Hubo Algun Error";
-	}else{
-		echo"Datos Guardados Correctamente";
-	}
-?>
+//conectamos Con el servidor
+$conectar=@mysqli_connect("localhost","root",'');
+//verificamos la conexion
+if(!$conectar){
+echo"No Se Pudo Conectar Con El Servidor";
+}else{
+$base=mysqli_select_db($conectar,"usuarios");
+if(!$base){
+echo"No Se Encontro La Base De Datos";
+}
+}
+//recuperar las variables
+$nombre=$_POST['nombre'];
+$dni=$_POST['dni'];
+$telefono=$_POST['telefono'];
+$fecha=$_POST['fecha'];
+$email=$_POST['email'];
+$contrasena=$_POST['contrasena'];
 
+//hacemos la sentencia de sql
+$sql="INSERT INTO Usuario (Nombre,DNI,Telefono,Fecha,Email,Contrasena) VALUES('$nombre','$dni','$telefono','$fecha','$email','$contrasena')";
+//ejecutamos la sentencia de sql
+$ejecutar=mysqli_query($conectar,$sql);
+//verificamos la ejecucion
+if(!$ejecutar){
+echo"Hubo Algun Error";
+}else{
+echo"Datos Guardados Correctamente";
+}
+?>
