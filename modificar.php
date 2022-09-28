9e92c0cc6818178c95e35ff4b0836dd18456c8b6
xballes <ballesxabi@gmail.com>
2022-09-28 10:28:18 +0200

    Modificar ya funciona

--- modificar.php
+++ modificar.php
@@ -19,22 +19,18 @@ $telefono=$_POST['telefono'];
 $fecha=$_POST['fecha'];
 $email=$_POST['email'];
 $contrasena=$_POST['contrasena'];
+$usuarioactual=$_SESSION['Usuario'];
+
+
 
 // si el campo no esta vacio, no actualizar los datos.
-if(!empty($nombre){
-    $nombresql="UPDATE Usuario SET Nombre='$nombre' where Nombre="$_SESSION['Usuario']"";
-}else if(!empty($dni)){
-    $dnisql="UPDATE Usuario SET Dni='$dni' where Nombre="$_SESSION['Usuario']"";
-}else if(!empty($telefono)){
-    $telefonosql="UPDATE Usuario SET Telefono='$telefono' where Nombre="$_SESSION['Usuario']"";
-}else if(!empty($fecha)){
-    $fechasql="UPDATE Usuario SET Fecha='$fecha' where Nombre="$_SESSION['Usuario']"";
-}
-else if(!empty($email)){
-    $emailsql="UPDATE Usuario SET Email='$email' where Nombre="$_SESSION['Usuario']"";
-}else if (!empty($contrasena)){
-    $contrasenasql="UPDATE Usuario SET Contrasena='$contrasena' where Nombre="$_SESSION['Usuario']"";
-}
+  
+    $nombresql="UPDATE Usuario SET Nombre='$nombre' WHERE (Nombre= '$usuarioactual') ";
+    $dnisql="UPDATE Usuario SET Dni='$dni' WHERE (Nombre='$usuarioactual') ";
+    $telefonosql="UPDATE Usuario SET Telefono='$telefono' WHERE Nombre='$usuarioactual'";
+    $fechasql="UPDATE Usuario SET Fecha='$fecha' WHERE Nombre='$usuarioactual'";
+    $emailsql="UPDATE Usuario SET Email='$email' WHERE Nombre='$usuarioactual'";
+    $contrasenasql="UPDATE Usuario SET Contrasena='$contrasena' WHERE Nombre='$usuarioactual'";
 
 //ejecutamos la sentencia de sql
 $ejecutar1=mysqli_query($conectar,$nombresql);
@@ -44,12 +40,62 @@ $ejecutar4=mysqli_query($conectar,$fechasql);
 $ejecutar5=mysqli_query($conectar,$emailsql);
 $ejecutar6=mysqli_query($conectar,$contrasenasql);
 //verificamos la ejecucion
-if(!$ejecutar){
+if(!$ejecutar1){
     ?> 
 	    	<h3 class="ok">¡Ha ocurrido un error,vuelve a introducir los datos!</h3>
            <?php
 }else{
-    echo"Datos Modificados Correctamente";
+    echo"Nombre Modificado Correctamente";
+    /*SESION*/
+    $_SESSION['Usuario']=$nombre;
+    header("Location:areapersonal.php");
+}
+if(!$ejecutar2){
+    ?> 
+	    	<h3 class="ok">¡Ha ocurrido un error,vuelve a introducir los datos!</h3>
+           <?php
+}else{
+    echo"DNI Modificado Correctamente";
+    /*SESION*/
+    $_SESSION['Usuario']=$nombre;
+    header("Location:areapersonal.php");
+}
+if(!$ejecutar3){
+    ?> 
+	    	<h3 class="ok">¡Ha ocurrido un error,vuelve a introducir los datos!</h3>
+           <?php
+}else{
+    echo"Telefono Modificado Correctamente";
+    /*SESION*/
+    $_SESSION['Usuario']=$nombre;
+    header("Location:areapersonal.php");
+}
+if(!$ejecutar1){
+    ?> 
+	    	<h3 class="ok">¡Ha ocurrido un error,vuelve a introducir los datos!</h3>
+           <?php
+}else{
+    echo"Nombre Modificado Correctamente";
+    /*SESION*/
+    $_SESSION['Usuario']=$nombre;
+    header("Location:areapersonal.php");
+}
+if(!$ejecutar1){
+    ?> 
+	    	<h3 class="ok">¡Ha ocurrido un error,vuelve a introducir los datos!</h3>
+           <?php
+}else{
+    echo"Nombre Modificado Correctamente";
+    /*SESION*/
+    $_SESSION['Usuario']=$nombre;
+    header("Location:areapersonal.php");
+}
+if(!$ejecutar1){
+    ?> 
+	    	<h3 class="ok">¡Ha ocurrido un error,vuelve a introducir los datos!</h3>
+           <?php
+}else{
+    echo"Nombre Modificado Correctamente";
     /*SESION*/
     $_SESSION['Usuario']=$nombre;
     header("Location:areapersonal.php");
