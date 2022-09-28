ebced8afd22e6d251d10c037da259795adba9319
xballes <ballesxabi@gmail.com>
2022-09-28 09:52:02 +0200

    Modificar .js

--- modificar.php
+++ modificar.php
@@ -20,20 +20,36 @@ $fecha=$_POST['fecha'];
 $email=$_POST['email'];
 $contrasena=$_POST['contrasena'];
 
-// si el campo esta vacio, no actualizar los datos.
-if(empty($nombre){
-
+// si el campo no esta vacio, no actualizar los datos.
+if(!empty($nombre){
+    $nombresql="UPDATE Usuario SET Nombre='$nombre' where Nombre="$_SESSION['Usuario']"";
+}else if(!empty($dni)){
+    $dnisql="UPDATE Usuario SET Dni='$dni' where Nombre="$_SESSION['Usuario']"";
+}else if(!empty($telefono)){
+    $telefonosql="UPDATE Usuario SET Telefono='$telefono' where Nombre="$_SESSION['Usuario']"";
+}else if(!empty($fecha)){
+    $fechasql="UPDATE Usuario SET Fecha='$fecha' where Nombre="$_SESSION['Usuario']"";
 }
-$sql="UPDATE Usuario SET Nombre='$nombre' where Nombre="$_SESSION['Usuario']"";
+else if(!empty($email)){
+    $emailsql="UPDATE Usuario SET Email='$email' where Nombre="$_SESSION['Usuario']"";
+}else if (!empty($contrasena)){
+    $contrasenasql="UPDATE Usuario SET Contrasena='$contrasena' where Nombre="$_SESSION['Usuario']"";
+}
+
 //ejecutamos la sentencia de sql
-$ejecutar=mysqli_query($conectar,$sql);
+$ejecutar1=mysqli_query($conectar,$nombresql);
+$ejecutar2=mysqli_query($conectar,$dnisql);
+$ejecutar3=mysqli_query($conectar,$telefonosql);
+$ejecutar4=mysqli_query($conectar,$fechasql);
+$ejecutar5=mysqli_query($conectar,$emailsql);
+$ejecutar6=mysqli_query($conectar,$contrasenasql);
 //verificamos la ejecucion
 if(!$ejecutar){
     ?> 
 	    	<h3 class="ok">¡Ha ocurrido un error,vuelve a introducir los datos!</h3>
            <?php
 }else{
-    echo"Datos Actualizados Correctamente";
+    echo"Datos Modificados Correctamente";
     /*SESION*/
     $_SESSION['Usuario']=$nombre;
     header("Location:areapersonal.php");
