a559f7ca713b44d9c6bccda4a5e8f24ac62429f1
xballes <ballesxabi@gmail.com>
2022-10-10 16:32:35 +0200

    ModificarUsuario()

--- script.js
+++ script.js
@@ -31,21 +31,6 @@ else if(/^\d{8}[a-zA-Z]$/.test(document.getElementById('dni').value)) {
     }
 }
 //-----------------------------------------------------------------------------------------------------------------
-function comprobarCamposPerro(){
-    if(document.getElementById('nombrePerro').value.length>0){
-        if((!isNaN(document.getElementById('nombrePerro').value))){ //Comprueba si el campo es vacio o contiene numeros ()
-            alert("El campo nombre esta vacio o contiene un numero");
-            return false;
-        }
-    }
-    if(document.getElementById('fechaPerro').value.length>0){ 
-                if(!(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(document.getElementById('fechaPerro').value))){ //https://foroayuda.es/php-regex-para-verificar-la-fecha-esta-en-formato-aaaa-mm-dd/
-                    alert("La fecha introducida no sigue el patron: aaaa-mm-dd");
-                    return false;
-                }
-            }
-    }
-
 //-------------------------------------------------------------------------------------------------------------------
 
 function registrarPerro(){
@@ -64,18 +49,23 @@ function registrarPerro(){
     }
 //-----------------------------------MODIFICAR--------------------------------------------
 function modificarUsuario(){
-    if(document.getElementById('nombreMod').value.lentgh>0){
-        if(!isNaN(document.getElementById('nombreMod').value)){
-            alert("El campo nombre contiene un numero");
+    if(document.getElementById('nombreMod').value.length>0){
+        if((!isNaN(document.getElementById('nombreMod').value))){ //Comprueba si el campo es vacio o contiene numeros ()
+            alert("El campo nombre esta vacio o contiene un numero");
             return false;
-         }
+        }
     }
-
-     if(document.getElementById('dniMod').value.lentgh>0){
-         if(/^\d{8}[a-zA-Z]$/.test(document.getElementById('dniMod').value)) {
+    if(document.getElementById('fechaMod').value.length>0){ 
+                if(!(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(document.getElementById('fechaMod').value))){ //https://foroayuda.es/php-regex-para-verificar-la-fecha-esta-en-formato-aaaa-mm-dd/
+                    alert("La fecha introducida no sigue el patron: aaaa-mm-dd");
+                    return false;
+                }
+            }
+    if(document.getElementById('dniMod').value.length>0){
+        if(/^\d{8}[a-zA-Z]$/.test(document.getElementById('dniMod').value)) {
             var n = document.getElementById('dniMod').value.substr(0,8);
             var c = document.getElementById('dniMod').value.substr(8,1);
-             if(!(c.toUpperCase() == 'TRWAGMYFPDXBNJZSQVHLCKET'.charAt(n%23))){
+            if(!(c.toUpperCase() == 'TRWAGMYFPDXBNJZSQVHLCKET'.charAt(n%23))){
                 alert('DNI erroneo');
                 return false;
             }
@@ -84,26 +74,23 @@ function modificarUsuario(){
                 return false;
         
             }
-        }        
-     if(document.getElementById('telefonoMod').value.lentgh>0){
-        if(document.getElementById('telefonoMod').value.length!=9){
-            alert("El numero debe ser de 9 digitos");
-            return false;
-
         }
-    }
-     if(document.getElementById('fechaMod').value.lentgh>0){
-        if(!(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(document.getElementById('fechaMod').value))){
-            alert("La fecha introducida no sigue el patron: aaaa-mm-dd");
-            return false;
-        }
-    }
-     if(document.getElementById('emailMod').value.lentgh>0){
-        if(!(/^[a-zA-Z]+([\.]?[a-zA-Z0-9_-]+)*@[a-z0-9]+([\.-]+[a-z0-9]+)*\.[a-z]{2,4}$/.test(document.getElementById('emailMod').value))){
-            alert("El email introducido no es correcto");
-            return false;
-        }
-    }
 }
+
+    function comprobarCamposPerro(){
+        if(document.getElementById('nombrePerro').value.length>0){
+            if((!isNaN(document.getElementById('nombrePerro').value))){ //Comprueba si el campo es vacio o contiene numeros ()
+                alert("El campo nombre esta vacio o contiene un numero");
+                return false;
+            }
+        }
+        if(document.getElementById('fechaPerro').value.length>0){ 
+                    if(!(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(document.getElementById('fechaPerro').value))){ //https://foroayuda.es/php-regex-para-verificar-la-fecha-esta-en-formato-aaaa-mm-dd/
+                        alert("La fecha introducida no sigue el patron: aaaa-mm-dd");
+                        return false;
+                    }
+                }
+        }
+
 
 
