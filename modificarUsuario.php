997c2d60841f07d46aa8286d803bc81aedd8284f
xballes <ballesxabi@gmail.com>
2022-10-10 16:46:44 +0200

    Modificar usuario ya funciona

--- modificarUsuario.php
+++ modificarUsuario.php
@@ -9,7 +9,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="formularios.css">
-    <script src="./script.js"></script>
+    <script src="./prueba.js"></script>
   <title>Modificacion de datos</title>
 </head>
 <body>
