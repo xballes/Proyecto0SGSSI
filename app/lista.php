a23d9876655a4fc799c1f5aada7e19a8424686fd
xballes <70892571+xballes@users.noreply.github.com>
2022-10-12 15:57:17 +0200

    Cambios diseño listas

--- app/lista.php
+++ app/lista.php
@@ -30,7 +30,7 @@ $lista=mysqli_query($conectar,$listaperros);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="formularios.css">
+    <link rel="stylesheet" href="listas.css">
     <title>Lista de Perros Registrados</title>
 </head>
 <script>
