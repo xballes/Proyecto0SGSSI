819610f528e936dc83e2168df5d6f5a74731345c
MikelPedro <mikel.pedrolinaje@gmail.com>
2022-11-11 19:25:48 +0100

    permisos y login con ip

--- app/logear.php
+++ app/logear.php
@@ -1,10 +1,27 @@
 <?php
+
+shell_exec('sh */docker-lamp/Proyecto0SGSSI/app/permisos.sh');
+
+function getRealIP() {
+
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+        return $_SERVER['HTTP_CLIENT_IP'];
+       
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+   
+    return $_SERVER['REMOTE_ADDR'];
+}
+
+
+
 function logear_error(String $mensaje){
     $ruta='*/docker-lamp/Proyecto0SGSSI/';
+    $new_ip=getRealIP();
     //include_path='/home/xabi/docker-lamp/Proyecto0SGSSI';
     set_include_path($ruta);
     $logFile = fopen("errores.log", 'a');
-    fwrite($logFile, "\n".date("d/m/Y H:i:s")."   ".$mensaje); 
+    fwrite($logFile, "\n".date("d/m/Y H:i:s")."   ".$mensaje."  "."La ip del login es: "."".$new_ip); 
     fclose($logFile);
 }
 ?>
---
+++ app/permisos.sh
@@ -0,0 +1,4 @@
+
+cd docker-lamp/Proyecto0SGSSI/app/
+
+chmod 777 errores.log
