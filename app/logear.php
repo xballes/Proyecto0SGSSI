78ec310b7fa45e3a10ed66dbafa323d761dfd3c8
xballes <70892571+xballes@users.noreply.github.com>
2022-11-16 17:42:11 +0100

    Loegar.php sube el log a la base de datos

--- app/logear.php
+++ app/logear.php
@@ -1,5 +1,6 @@
 <?php
 shell_exec('sh */docker-lamp/Proyecto0SGSSI/app/permisos.sh');
+
 function getRealIP() {
 
     if (!empty($_SERVER['HTTP_CLIENT_IP']))
@@ -12,6 +13,15 @@ function getRealIP() {
 }
 
 function logear_error(String $mensaje){
+    $conectar=@mysqli_connect("db","lK9pF81rtVq1","o80dGpAMjKb2","database");
+    if(!$conectar){
+    echo"No Se Pudo Conectar Con El Servidor";
+    }else{
+        $base=mysqli_select_db($conectar,"database");
+            if(!$base){
+                echo"No Se Encontro La Base De Datos";
+            }
+    }
     shell_exec('sh */docker-lamp/Proyecto0SGSSI/app/permisos.sh');
     $ruta='*/docker-lamp/Proyecto0SGSSI/app/';
     $new_ip=getRealIP();
@@ -20,6 +30,12 @@ function logear_error(String $mensaje){
     $logFile = fopen("errores.log", 'a+b');
     fwrite($logFile, "\n".date("d/m/Y H:i:s")."   ".$mensaje."  "."La ip del login es: "."".$new_ip); 
     fclose($logFile);
+    if($log=$conectar->prepare("INSERT INTO Log VALUES(?,?)")){
+        $log->bind_param('ss',htmlspecialchars(mysqli_real_escape_string($conectar,$mensaje)),htmlspecialchars(mysqli_real_escape_string($conectar,$new_ip)));
+        $log->execute();
+        $log->close();
+    }
 }
+
 
 ?>
